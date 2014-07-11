e2cb761e6668fef3ec4cab26d8e2cd73802bd229
Ayush Ladia <ayush.ladia@chronus.com>
2014-07-11 21:17:46 +0530

    Finish sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  
+  
+  protect_from_forgery
+  include SessionsHelper
+
+  # Force signout to prevent CSRF attacks
+  def handle_unverified_request
+    sign_out
+    super
+  end
+
+  def create
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+
+end
