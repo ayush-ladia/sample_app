ab9e03617115f4484ae304ec181ec13be61f16d8
Ayush Ladia <ayush.ladia@chronus.com>
2014-07-11 17:02:46 +0530

    Tester

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 SampleApp::Application.routes.draw do
 
-   resources :users
-
    root to: 'static_pages#home'
+   
+   resources :users
+   resources :sessions, only: [:new, :create, :destroy]
 
    match '/signup',  to: 'users#new'
 
